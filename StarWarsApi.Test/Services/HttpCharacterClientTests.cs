93b06886c090fe8552f55bca74c6c423214442e2
benlewis9000 <benjaminlewis9000@gmail.com>
2024-02-08 19:28:23 +0000

    Add HttpClientCharacterTests

---
+++ StarWarsApi.Test/Services/HttpCharacterClientTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework.Legacy;
+using StarWarsApi.Services;
+
+namespace StarWarsApi.Test.Services;
+
+internal class HttpCharacterClientTests
+{
+    [Test]
+    public async Task Fetch_SuccessfullyFetchesCharacters()
+    {
+        ICharacterClient client = new HttpCharacterClient();
+        var characters = (await client.FetchAsync()).ToList();
+        Assert.That(characters.Count(), Is.EqualTo(10));
+        var luke = characters.First();
+        Assert.That(luke.Name, Is.EqualTo("Luke Skywalker"));
+        Assert.That(luke.BirthYear, Is.EqualTo("19BBY"));
+        Assert.That(luke.HomeWorld, Is.EqualTo("Tatooine"));
+        Assert.That(luke.FilmCount, Is.EqualTo(4));
+    }
+}
