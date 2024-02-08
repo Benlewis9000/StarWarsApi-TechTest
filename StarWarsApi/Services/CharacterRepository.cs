258caedb4c3704deab790d444001e4837de97a21
benlewis9000 <benjaminlewis9000@gmail.com>
2024-02-08 18:55:00 +0000

    Add ICharacterRepository and impl

---
+++ StarWarsApi/Services/CharacterRepository.cs
@@ -0,0 +1,30 @@
+﻿using TestWebApi.Model;
+
+namespace TestWebApi.Services;
+
+public class CharacterRepository : ICharacterRepository
+{
+    private readonly ICharacterClient _characterClient;
+    private List<Character> _characters = new();
+    private bool _isInitialised = false;
+
+    public CharacterRepository(ICharacterClient characterClient)
+    {
+        _characterClient = characterClient;
+    }
+
+    public async Task<IEnumerable<Character>> GetAll()
+    {
+        if (!_isInitialised)
+        {
+            await Update();
+            _isInitialised = true;
+        }
+        return _characters;
+    }
+
+    public async Task Update()
+    {
+        _characters = (await _characterClient.FetchAsync()).ToList();
+    }
+}
---
+++ StarWarsApi/Services/ICharacterRepository.cs
@@ -0,0 +1,14 @@
+﻿using TestWebApi.Model;
+
+namespace TestWebApi.Services;
+
+public interface ICharacterRepository
+{
+    /// <summary>
+    /// Get all <see cref="Character"/> instances stored in the repository
+    /// </summary>
+    /// <returns>All character models</returns>
+    Task<IEnumerable<Character>> GetAll();
+
+    Task Update();
+}
