8c6bf50146822bf6a42f24a8d7bf9cf3663b0398
benlewis9000 <benjaminlewis9000@gmail.com>
2024-02-08 19:07:00 +0000

    Add CharacterRepositoryTests

---
+++ StarWarsApi.Test/Services/CharacterRepositoryTests.cs
@@ -0,0 +1,37 @@
+﻿using NSubstitute;
+using NUnit.Framework.Legacy;
+using StarWarsApi.Model;
+using StarWarsApi.Services;
+
+namespace StarWarsApi.Test.Services;
+
+internal class CharacterRepositoryTests
+{
+    [Test]
+    public async Task GetAll_ReturnsCharacters()
+    {
+        ICharacterClient characterClient = Substitute.For<ICharacterClient>();
+        Character character = new() { Name = "Bob", BirthYear = "1999", HomeWorld = "Earth", FilmCount = 6 };
+        characterClient.FetchAsync().Returns(new List<Character>() { character, character });
+
+        ICharacterRepository repository = new CharacterRepository(characterClient);
+        var result = (await repository.GetAll()).ToList();
+        Assert.That(result.Count(), Is.EqualTo(2));
+        StringAssert.AreEqualIgnoringCase("Bob", result.First().Name);
+    }
+
+    [Test]
+    public async Task GetAll_OnlyFetchesDataOnce()
+    {
+        ICharacterClient characterClient = Substitute.For<ICharacterClient>();
+        Character character = new() { Name = "Bob", BirthYear = "1999", HomeWorld = "Earth", FilmCount = 6 };
+        characterClient.FetchAsync().Returns(new List<Character>() { character, character });
+
+        ICharacterRepository repository = new CharacterRepository(characterClient);
+        await repository.GetAll();
+        await characterClient.Received(1).FetchAsync();
+        await repository.GetAll();
+        characterClient.ClearReceivedCalls();
+        await characterClient.DidNotReceive().FetchAsync();
+    }
+}
