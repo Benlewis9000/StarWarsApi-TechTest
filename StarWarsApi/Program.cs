45b66024050c8c190332fd743f996b9f45ada58c
benlewis9000 <benjaminlewis9000@gmail.com>
2024-02-08 18:57:23 +0000

    Register services

--- StarWarsApi/Program.cs
+++ StarWarsApi/Program.cs
@@ -1,3 +1,5 @@
+using TestWebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ICharacterClient, HttpCharacterClient>();
+builder.Services.AddSingleton<ICharacterRepository, CharacterRepository>();
 
 var app = builder.Build();
 
