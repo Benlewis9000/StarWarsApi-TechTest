8eaf507d85ea084c2cc8be40e99cc4a1c7b2c743
benlewis9000 <benjaminlewis9000@gmail.com>
2024-02-08 18:52:21 +0000

    Add Character model

---
+++ StarWarsApi/Model/Character.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestWebApi.Model;
+
+public class Character
+{
+    public string Name { get; set; } = string.Empty;
+    public string BirthYear { get; set; }
+    public string HomeWorld { get; set; } = string.Empty;
+    [Range(0, int.MaxValue)]
+    public int FilmCount { get; set; }
+}
