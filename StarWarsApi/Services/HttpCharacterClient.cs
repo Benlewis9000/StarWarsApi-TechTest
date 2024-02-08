8bfd2b39c9c40be0f58c9cd632b829b7cbb6a6b4
benlewis9000 <benjaminlewis9000@gmail.com>
2024-02-08 18:54:18 +0000

    Add ICharacterClient and Http implementation

---
+++ StarWarsApi/Services/HttpCharacterClient.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using TestWebApi.Model;
+
+namespace TestWebApi.Services;
+
+public class HttpCharacterClient : ICharacterClient
+{
+    private readonly Uri _swapiEndpoint = new Uri("https://swapi.dev/api/");
+
+    public async Task<IEnumerable<Character>> FetchAsync()
+    {
+        Uri peopleEndpoint = new Uri(_swapiEndpoint, "people");
+
+        // Get character data from SWAPI
+        using var client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(peopleEndpoint);
+        response.EnsureSuccessStatusCode();
+        string json = await response.Content.ReadAsStringAsync();
+        List<CharacterResponse> characterResponses = JsonConvert.DeserializeObject<ResponseRoot<CharacterResponse>>(json).Results;
+
+        // Fetch homeworld data and update character info in turn
+        foreach (CharacterResponse character in characterResponses)
+        {
+            response = await client.GetAsync(character.Homeworld);
+            response.EnsureSuccessStatusCode();
+            json = await response.Content.ReadAsStringAsync();
+            character.Homeworld = JsonConvert.DeserializeObject<PlanetResponse>(json).Name;
+        }
+
+        // TODO catch http and json exceptions
+
+        return characterResponses.Select(r => new Character
+            { Name = r.Name, BirthYear = r.BirthYear, HomeWorld = r.Homeworld, FilmCount = r.Films.Count });
+    }
+
+    // Extract only what is needed from JSON responses
+    private class ResponseRoot<T>
+    {
+        public List<T> Results { get; set; }
+    }
+
+    private class CharacterResponse
+    {
+        public string Name { get; set; }
+        [JsonProperty("birth_year")]
+        public string BirthYear { get; set; }
+        public string Homeworld { get; set; }
+        public List<string> Films { get; set; }
+    }
+
+    private class PlanetResponse
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ StarWarsApi/Services/ICharacterClient.cs
@@ -0,0 +1,12 @@
+﻿using TestWebApi.Model;
+
+namespace TestWebApi.Services;
+
+public interface ICharacterClient
+{
+    /// <summary>
+    /// Build <see cref="Character"/> models with data fetched from SWAPI
+    /// </summary>
+    /// <returns>Characters from SWAPI data</returns>
+    Task<IEnumerable<Character>> FetchAsync();
+}
