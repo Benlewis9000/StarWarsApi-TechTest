8fd99c1f0e7181cfe0cefa828b3097b6fdcdc9c2
benlewis9000 <benjaminlewis9000@gmail.com>
2024-02-09 00:48:03 +0000

    Add CharacterList and CharacterRow

---
+++ StarWarsClient/app/src/app/character-list.tsx
@@ -0,0 +1,69 @@
+"use client"
+import React, { useCallback } from "react";
+import { useEffect, useState } from "react";
+import { Character } from "./character";
+import { getCharacters } from "./api-client";
+import { get } from "http";
+import { CharacterRow } from "./character-row";
+
+const CharacterList = () => {
+    const [characters, setCharacters] = React.useState<Character[]>([]);
+    const [isLoading, setIsLoading] = React.useState<boolean>(true);
+    const [errorMessage, setErrorMessage] = React.useState<string>("");
+    const [isErrored, setIsErrored] = React.useState<boolean>(false);
+    
+    useEffect(() => {
+        setIsLoading(true);
+        const fetchCharacters = async () => {
+            const response = await getCharacters();
+            //setIsLoading(false);
+            if (response.data === undefined){
+                setIsErrored(true);
+                setErrorMessage(response.error);
+            }
+            else {
+                setCharacters(response.data);
+            }
+        }
+
+        fetchCharacters();
+    }, [])
+
+    return (
+        <>
+            <div className="row mb-2">
+                <h5 className="themeFontColor text-center">
+                    Characters
+                </h5>
+            </div>
+            <div>
+                <p>{isLoading}</p>
+            {isLoading ?? <p>Loading...</p>}
+            </div>
+            {isErrored ? (
+                <p>{errorMessage}</p>
+                ) : (
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Birth Year</th>
+                            <th>Homeworld</th>
+                            <th>Film Appearances</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {isLoading ?? <p>Loading...</p>}
+                        {isErrored ? (
+                            <p>{errorMessage}</p>
+                        ) : (
+                            characters.map(character => <CharacterRow key={character.name} character={character} />)
+                        )}
+                    </tbody>
+                </table>
+            )}
+        </>
+    );
+}
+
+export { CharacterList }
---
+++ StarWarsClient/app/src/app/character-row.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Character } from "./character";
+
+interface CharacterItemProps {
+    key: string,
+    character: Character
+}
+
+const CharacterRow: React.FunctionComponent<CharacterItemProps> = ({key, character}) => {
+    console.log(character);
+    return (
+        <tr key={key}>
+            <td>{character.name}</td>
+            <td>{character.birthYear}</td>
+            <td>{character.homeWorld}</td>
+            <td>{character.filmCount}</td>
+        </tr>
+    )
+}
+
+export { CharacterRow }
